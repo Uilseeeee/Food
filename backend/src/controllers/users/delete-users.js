9f7959d0651dcdd12fc8e6b64f768d691e2cd7f0
Uilsee <uilseeu437@gmail.com>
2025-02-27 12:37:54 +0800

    l

--- backend/src/controllers/users/delete-users.js
+++ backend/src/controllers/users/delete-users.js
@@ -1,1 +1,12 @@
-import express from "express";
+import mongoose from "mongoose";
+import { Users } from "../../models/user.schema.js";
+
+export const DeleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteUser = await Users.findByIdAndDelete(id);
+        res.json({ message: "User deleted successfully", user: deleteUser });
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+    };
--- backend/src/routes/users-router.js
+++ backend/src/routes/users-router.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { userSignup } from "../controllers/users/create-users.js";
+import { DeleteUser } from "../controllers/users/delete-users.js";
 
 export const userRouter = express.Router();
 
 userRouter.post("/", userSignup);
+userRouter.delete("/:id", DeleteUser);
