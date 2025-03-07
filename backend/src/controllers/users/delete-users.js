91268b9b7af496736ff50342aced3921f98c0bc8
Uilsee <uilseeu437@gmail.com>
2025-03-07 21:13:04 +0800

    s

--- backend/src/controllers/users/delete-users.js
+++ backend/src/controllers/users/delete-users.js
@@ -12,6 +12,17 @@ export const DeleteUser = async (req, res) => {
     } catch (err) {
         res.status(404).json({ message: err.message });
     }
+    try {
+        const { id } = req.params;
+        const deleteUser = await Users.findByIdAndDelete(id);
+        res.json({ message: "User deleted successfully", user: deleteUser });
+
+        if (!deleteUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
     
    
     };
