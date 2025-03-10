409df270a5d25ebe283a0c41f53cfda1ac33510b
Uilsee <uilseeu437@gmail.com>
2025-03-10 11:34:56 +0800

    first

--- backend/src/models/FoodOrderItems-schema.js
+++ backend/src/models/FoodOrderItems-schema.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const { Schema } = mongoose.Schema;
 
 export const orderItemsSchema = new Schema(
-{
-food: { type : SchemaType.ObjectId, required : true, ref : 'Foods'},
-quantity: { type : Number , required : true },
-},
-{
-    _id : false
-}
+  {
+    food: { type: SchemaType.ObjectId, required: true, ref: "Foods" },
+    quantity: { type: Number, required: true },
+  },
+  {
+    _id: false,
+  }
 );
