ecd54713490f124486697918bd20bf6097ace323
Uilsee <uilseeu437@gmail.com>
2025-03-13 09:49:54 +0800

    first

--- backend/src/controllers/users/update-users.js
+++ backend/src/controllers/users/update-users.js
@@ -1,1 +1,23 @@
 import express from "express";
+import { Users } from "../../models/user.schema.js";
+
+export const updateUsers = async (req, res) => {
+    const { id } = req.params;
+    const { email, password, phonenumber, address } = req.body;
+    
+    try {
+        const updateUser = await Users.findByIdAndUpdate(id, {
+        email,
+        password,
+        phonenumber,
+        address,
+        role,
+        orderedFood,
+        createdAt,
+        updatedAt
+        });
+        res.json({ message: "User updated successfully", user: updateUser });
+    } catch (err) {
+        res.json({ message: "Error occured" });
+    }
+    }
