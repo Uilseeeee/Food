24c4480684f37c5f8babaf6dba391023842faf67
Uilsee <uilseeu437@gmail.com>
2025-03-19 11:20:34 +0800

    second

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,5 +1,6 @@
 import { Input } from "./ui/input";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export const Login = () => {
   return (
@@ -7,9 +8,16 @@ export const Login = () => {
       <div className="w-5/6 flex flex-row items-center justify-between">
         <div>
           <h1 className="text-xl font-bold">Create account</h1>
-          <h2 className="text-[#71717A]">Sign up to explore your favorite dishes.</h2>
+          <h2 className="text-[#71717A]">
+            Sign up to explore your favorite dishes.
+          </h2>
           <Input type="text" placeholder="Enter your email address" />
-          <Input type="password" placeholder="Password" />
+
+          <Button className="bg-gray-500 w-72">Lets go</Button>
+          <div className="flex flex-row items-center justify-between">
+            <h2>Already have an account ?</h2>
+            <Button className="bg-white text-black">Login</Button>
+          </div>
         </div>
         <Image src="/delivery.png" alt="Logo" width={700} height={100} />
       </div>
