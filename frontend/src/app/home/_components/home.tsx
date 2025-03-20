d0a3e390117f9b7cff8f4e6919f01f82806c4fc6
Uilsee <uilseeu437@gmail.com>
2025-03-20 12:13:51 +0800

    fourth

--- frontend/src/app/home/_components/home.tsx
+++ frontend/src/app/home/_components/home.tsx
@@ -1,9 +1,18 @@
-
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export const Home = () => {
-    return (
-        <div>
-        <h1>Home</h1>
+  return (
+    <div className="flex flex-col bg-[#404040]">
+      <Image src="/home.png" alt="Logo" width={2000} height={150} />
+      <div className="w-screen h-28 flex justify-around flex-col">
+        <div className="w-11/12 flex self-center">
+          <p className="text-2xl text-white font-bold">Categories</p>
         </div>
-    );
-}
+        <div className="w-11/12 flex self-center">
+        <Button className="rounded-4xl">button</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
