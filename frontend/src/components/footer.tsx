f05ca3747e14ea52a5f1b175a4a9905ebf496435
Uilsee <uilseeu437@gmail.com>
2025-03-24 10:05:03 +0800

    second

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,12 +1,21 @@
-// import Image from "next/image";
+import Image from "next/image";
 // import Link from "next/link";
-
 // import { Button } from "@/components/ui/button";
 
 export const Footer = () => {
   return (
-    <footer className="w-full h-60 bg-black">
+    <footer className="w-full h-60 bg-black ">
+        <div className="w-full h-60 flex justify-center items-center">
+      <Image src="/footer.png" alt="Logo" width={1500} height={200} />
+      <div className="w-4/5 flex justify-center bg-red-400 self-center">
+        <div>
+            <Image src="/footerLogo.png" alt="Logo" width={100} height={100} />
+        </div>
+        <div>
 
+        </div>
+      </div>
+      </div>
     </footer>
   );
 };
