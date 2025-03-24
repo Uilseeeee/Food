99650468c6a62770c9a2188ca6f7f6cb9376a95b
Uilsee <uilseeu437@gmail.com>
2025-03-24 09:27:22 +0800

    first

---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+// import Link from "next/link";
+
+// import { Button } from "@/components/ui/button";
+
+export const Footer = () => {
+  return (
+    <footer className="w-full h-60 bg-black">
+
+    </footer>
+  );
+};
