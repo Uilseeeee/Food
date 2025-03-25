66caa547d9b55e5bba17a569618622f0eba7f64b
Uilsee <uilseeu437@gmail.com>
2025-03-25 10:26:48 +0800

    first?

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -4,17 +4,24 @@ import Image from "next/image";
 
 export const Footer = () => {
   return (
-    <footer className="w-full h-60 bg-black ">
-        <div className="w-full h-60 flex justify-center items-center">
-      <Image src="/footer.png" alt="Logo" width={1500} height={200} />
-      <div className="w-4/5 flex justify-center bg-red-400 self-center">
-        <div>
-            <Image src="/footerLogo.png" alt="Logo" width={100} height={100} />
-        </div>
-        <div>
-
-        </div>
+    <footer className="w-screen h-fit bg-black flex flex-col justify-center">
+      <div className="w-full h-60 flex justify-center items-center">
+        <Image src="/footer.png" alt="Logo" width={1500} height={200} />
       </div>
+      <div className="w-11/12 bg-black flex self-center justify-between">
+        <Image src="/footerLogo.png" alt="Logo" width={100} height={100} />
+        <div className="flex flex-col">
+            <h4 className="text-[#71717A]">NOMNOM</h4>
+        </div>
+        <div className="flex flex-col">
+            <h4 className="text-[#71717A]">NOMNOM</h4>
+        </div>
+        <div className="flex flex-col">
+            <h4 className="text-[#71717A]">NOMNOM</h4>
+        </div>
+        <div className="flex flex-col">
+            <h4 className="text-[#71717A]">NOMNOM</h4>
+        </div>
       </div>
     </footer>
   );
