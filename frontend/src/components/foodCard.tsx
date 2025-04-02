9f367cab183064b2a90ebaec4bf094f5384da1b3
Uilsee <uilseeu437@gmail.com>
2025-04-02 21:18:17 +0800

    s

--- frontend/src/components/foodCard.tsx
+++ frontend/src/components/foodCard.tsx
@@ -5,11 +5,13 @@ import Image from "next/image";
 // import { FoodOrder } from "./foodOrder";
 import axios from "axios";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-// import { Button } from "./ui/button";
+import { Button } from "./ui/button";
+
 
 type FoodType = {
   name: [];
   category: [];
+  price: number;
 };
 export const FoodCard = () => {
   const [food, setFood] = useState<FoodType[]>([]);
@@ -41,30 +43,43 @@ export const FoodCard = () => {
             <Dialog key={index}>
               <DialogTrigger className=" left-0 bottom-0 rounded-full w-full">
                 <div className="flex flex-row w-100 justify-center">
-                  <div className="h-60 w-72 bg-white flex items-center justify-center flex-col">
+                  <div className="h-60 w-72 bg-white flex items-center justify-center flex-col rounded-2xl">
                     <div className="relative">
-                       <Image
-                      className="rounded-2xl"
-                      src="/food2.png"
-                      alt="Logo"
-                      width={265}
-                      height={200}
-                    />
+                      <Image
+                        className="rounded-2xl"
+                        src="/food2.png"
+                        alt="Logo"
+                        width={265}
+                        height={200}
+                      />
                     </div>
-                   
+
                     {/* <Button className="rounded-full absolute right-0 bottom-0" variant="outline">
                       +
                     </Button> */}
-                    <div className="text-[#EF4444] font-bold text-[18px]">
-                      {" "}
-                      {food.name}
+                    <div className="flex flex-row items-center w-64 justify-between">
+                      <div className="text-[#EF4444] font-semibold text-[18px]">
+                        {" "}
+                        {food.name}
+                      </div>
+                      <div>${food.price}</div>
                     </div>
                   </div>
                 </div>
               </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px]">
+              <DialogContent className="sm:max-w-[425px] flex flex-row">
                 <Image src="/food2.png" alt="Logo" width={200} height={200} />
-                {food.name}
+
+                <div className="">
+                  <div className="text-[#EF4444] font-semibold">
+                    {food.name}
+                  </div>
+                  <div>
+                    <div>Total price</div>
+                    <div>{food.price}</div>
+                  </div>
+                  <Button className="w-44 mt-10">Add to cart</Button>
+                </div>
               </DialogContent>
             </Dialog>
           ))}
--- frontend/src/components/foodOrder.tsx
+++ frontend/src/components/foodOrder.tsx
@@ -1,9 +1,25 @@
-// "use client";
+"use client";
 
-// import React from "react";
-// import { Button } from "@/components/ui/button";
+import React from "react";
 // import axios from "axios";
 // import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
 // export const FoodOrder = () => {
 //   const [error, setError] = useState("");
@@ -25,25 +41,60 @@
 //     return <div>{error}</div>;
 //   }
 
-//   return (
-//     <div>
-//       <Dialog>
-//         <DialogTrigger
-//           className="absolute left-0 bottom-0 rounded-full w-9"
-//           asChild
-//         >
-//           <Button variant="outline">+</Button>
-//         </DialogTrigger>
-//         <DialogContent className="sm:max-w-[425px]">
-//           {food.slice(0, 5).map((food, index) => (
-//             <div key={index}>
-//               {" "}
-//               <Image src="/food2.png" alt="Logo" width={200} height={200} />
-//               {food.name}
-//             </div>
-//           ))}
-//         </DialogContent>
-//       </Dialog>
-//     </div>
-//   );
-// };
+
+export function TabsDemo() {
+  return (
+    <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="account">Account</TabsTrigger>
+        <TabsTrigger value="password">Password</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <Card>
+          <CardHeader>
+            <CardTitle>Account</CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when youre done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" defaultValue="Pedro Duarte" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" defaultValue="@peduarte" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="password">
+        <Card>
+          <CardHeader>
+            <CardTitle>Password</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, youll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="current">Current password</Label>
+              <Input id="current" type="password" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="new">New password</Label>
+              <Input id="new" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  )
+}
