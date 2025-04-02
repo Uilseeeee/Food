92d24fa88801fe3c5f16f88b93b681e14e803afc
Uilsee <uilseeu437@gmail.com>
2025-04-02 10:28:23 +0800

    first

--- frontend/src/components/foodCard.tsx
+++ frontend/src/components/foodCard.tsx
@@ -1,22 +1,66 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import { FoodOrder } from "./foodOrder";
+// import { FoodOrder } from "./foodOrder";
+import axios from "axios";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "./ui/button";
 
+type  FoodType ={
+  name: string
+  
+}
 export const FoodCard = () => {
+  const [food, setFood] = useState<FoodType[]>([]);
+  const [error, setError] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/foods");
+      setFood(response.data);
+    } catch (err) {
+      console.log(err);
+      setError("Error occurred");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
     <div>
-      <div className="w-[265px] relative">
-        <Image
-          className=""
-          src="/spaghetti.png"
-          alt="Logo"
-          width={265}
-          height={200}
-        />
+      <div className="h-80 flex items-center relative">
+        <div className="h-80 flex items-center">
+          <Dialog>
+            <DialogTrigger className="absolute left-0 bottom-0 rounded-full w-9">
+              <div>
+                {food.slice(0, 5).map((food, index) => (
+                  <div key={index}>
+                    <Image
+                      className="rounded-2xl"
+                      src="/food2.png"
+                      alt="Logo"
+                      width={265}
+                      height={200}
+                    />
+                    <Button className="rounded-full" variant="outline">+</Button>
+                    {food.name}
+                  </div>
+                ))}
+              </div>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                  <Image src="/food2.png" alt="Logo" width={200} height={200} />
+            </DialogContent>
+          </Dialog>
+        </div>
       </div>
-      <FoodOrder />
     </div>
   );
 };
--- frontend/src/components/foodOrder.tsx
+++ frontend/src/components/foodOrder.tsx
@@ -1,53 +1,49 @@
-"use client";
+// "use client";
 
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import React from "react";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import axios from "axios";
-import { useState, useEffect } from "react";
+// import React from "react";
+// import { Button } from "@/components/ui/button";
+// import axios from "axios";
+// import { useState, useEffect } from "react";
 
-export const FoodOrder = () => {
-  const [error, setError] = useState("");
-  const [food, setFood] = useState<food[]>([]);
+// export const FoodOrder = () => {
+//   const [error, setError] = useState("");
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get("http://localhost:8000/foods");
-      setFood(response.data);
-    } catch (err) {
-      console.error(err);
-      setError("error occurred.");
-    }
-  };
+//   const fetchData = async () => {
+//     try {
+//       const response = await axios.get("http://localhost:8000/foods");
+//     } catch (err) {
+//       console.error(err);
+//       setError("error occurred.");
+//     }
+//   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
 
-  if (error) {
-    return <div>{error}</div>;
-  }
+//   if (error) {
+//     return <div>{error}</div>;
+//   }
 
-  return (
-    <div>
-        <div>
-            {food.slice(0, 5).map((food, index) => (
-              <div key={index}>{food.name}</div>
-            ))}
-          </div>
-      <Dialog>
-        <DialogTrigger
-          className="absolute left-0 bottom-0 rounded-full w-9"
-          asChild
-        >
-          <Button variant="outline">+</Button>
-        
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <Image src="/spaghetti.png" alt="Logo" width={200} height={200} />
-        </DialogContent>
-      </Dialog>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <Dialog>
+//         <DialogTrigger
+//           className="absolute left-0 bottom-0 rounded-full w-9"
+//           asChild
+//         >
+//           <Button variant="outline">+</Button>
+//         </DialogTrigger>
+//         <DialogContent className="sm:max-w-[425px]">
+//           {food.slice(0, 5).map((food, index) => (
+//             <div key={index}>
+//               {" "}
+//               <Image src="/food2.png" alt="Logo" width={200} height={200} />
+//               {food.name}
+//             </div>
+//           ))}
+//         </DialogContent>
+//       </Dialog>
+//     </div>
+//   );
+// };
